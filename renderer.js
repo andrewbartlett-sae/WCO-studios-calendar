dda977080ad4f93926690962072291216266a691
andrewbartlett-sae <andrew.bartlett@sae.edu.au>
2025-08-22 17:04:07 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@ let feeds = [];
 let currentDate = new Date();
 const startHour = 8;
 const endHour = 21;
-const version = "v1.0"; // Version number
+const version = "v1.01"; // Version number
 
 // Track ongoing fetches
 let currentAbortController = null;
